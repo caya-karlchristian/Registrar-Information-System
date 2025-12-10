4f19ce679ae14ac6b7bb97f5ef2e532453cd5338
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2025-12-10 20:28:19 +0800

    Set up initial project files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    // Remove padding from here
+    <div className="overflow-hidden">
+      <p className="text-3xl font-bold underline">LESGOW TECH4WARDDDD!!!</p>
+    </div>
+  )
+}
+
+export default App
