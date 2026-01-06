ef8d3d5d93d9d7591b1ea1acb3753ca6a91c5576
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-01-06 22:59:47 +0800

    Might kaboom

--- registrar-backend/app/Models/StudentProfile.php
+++ registrar-backend/app/Models/StudentProfile.php
@@ -1,3 +1,9 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
 class StudentProfile extends Model
 {
     protected $table = 'student_profile';
@@ -5,11 +11,12 @@ class StudentProfile extends Model
     public $timestamps = false;
 
     protected $fillable = [
-        'user_id','first_name','middle_name','last_name',
-        'date_of_birth','permanent_address','contact_number'
+        'user_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'date_of_birth',
+        'permanent_address',
+        'contact_number'
     ];
-
-    public function academicRecord() {
-        return $this->hasOne(StudentAcademicRecord::class, 'student_profile_id');
-    }
 }
