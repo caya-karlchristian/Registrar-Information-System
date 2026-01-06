003f6190691d2786b0a1668ae3e45517cbd534fa
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-01-06 21:16:09 +0800

    Create class model in StudenAcademicRecord.php

---
+++ registrar-backend/app/Models/StudentAcademicRecord.php
@@ -0,0 +1,12 @@
+class StudentAcademicRecord extends Model
+{
+    protected $table = 'student_academic_record';
+    protected $primaryKey = 'academic_record_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'student_profile_id','student_number','course','year_level',
+        'school_year_admitted','last_school_year_attended',
+        'has_honorable_dismissal','graduation_date'
+    ];
+}
