c7566b6faf3573fbb776a6f681764186698a1c37
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2026-02-19 20:14:03 +0800

    Remove logout, delete account, and edit button on profilepage

--- src/layouts/DocumentLists.jsx
+++ src/layouts/DocumentLists.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 const DocumentLists = () => {
