c72bdadced7024f54ca773f14b9f51b0aa4052f6
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-02-23 09:04:47 +0800

    Try to fix dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
               <RequestForm />
             } /> {/* /student */}
           <Route path="home" element={
-              <StudentDashboard />
+                <StudentDashboard />
           } />
           <Route path="request" element={
             <RequestForm />
