f570365861acf26e05f494d915a21bb6a997785d
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-03-03 00:08:46 +0800

    Separate AuthService

---
+++ registrar-frontend/src/services/authService.js
@@ -0,0 +1,13 @@
+import api from "../API";
+
+export const loginRequest = (email, password) => {
+  return api.post("/login", { email, password });
+};
+
+export const fetchCurrentUser = () => {
+  return api.get("/me");
+};
+
+export const logoutRequest = () => {
+  return api.post("/logout");
+};
