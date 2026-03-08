4969d3a562a6409c2bc50ff3aee6e1a2ecc01462
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-03-08 21:43:45 +0800

    Add callback route in api.php

--- registrar-backend/routes/api.php
+++ registrar-backend/routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\RequestDocumentController;
 use App\Http\Controllers\RequestHistoryController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuditLogController;
+use App\Http\Controllers\SsoCallbackController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use App\Http\Controllers\AuditLogController;
 | Throttled to 10 attempts/min to prevent brute force on login
 |--------------------------------------------------------------------------
 */
+
+Route::post('/auth/callback', [SsoCallbackController::class, 'handle']);
 
 Route::post('/login', [AuthController::class, 'login'])
     ->middleware('throttle:10,1');
