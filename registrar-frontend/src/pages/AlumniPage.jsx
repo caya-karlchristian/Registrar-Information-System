c06d789edb746039e34656d52f53f1d80409e108
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2026-03-12 00:00:56 +0800

    Update frontend headers

--- registrar-frontend/src/pages/AlumniPage.jsx
+++ registrar-frontend/src/pages/AlumniPage.jsx
@@ -1,13 +1,12 @@
-import { Outlet} from 'react-router-dom';
+import { Outlet, useNavigate} from 'react-router-dom';
 import Header from '../components/Header.jsx';
 import AlumniNavigation from '../components/AlumniNavigation.jsx';
 import { useState, useEffect } from "react";
-// import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
-// Comment out - Not yet implemented (Email)
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
 
 const AlumniPage = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
     useEffect(() => {
       if (isMobileMenuOpen) {
@@ -39,12 +38,12 @@ const AlumniPage = () => {
               <Outlet />
             </div>
           </main>
-          {/* <button
+          <button
             onClick={() => navigate('/alumni/contact')} // This directs the main content to display the email design
-            className="fixed bottom-8 right-8 z-50 bg-pup-dark-maroon p-4 rounded-full shadow-2xl hover:scale-110 transition-all active:scale-95 group border-2 border-white/20"
+            className="fixed bottom-2 right-8 z-50 bg-pup-dark-maroon p-4 rounded-full shadow-2xl hover:scale-110 transition-all active:scale-95 group border-2 border-white/20"
             >
-            <ChatBubbleLeftRightIcon className="w-5 h-5 text-white" />
-        </button> */} 
+            <ChatBubbleLeftRightIcon className="w-4 h-4 text-white" />
+        </button> 
         </div>
       </div>
     );
