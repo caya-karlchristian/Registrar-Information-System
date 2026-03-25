dbf8bb63d2a024736eca4e14febe63323ddf865d
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-03-25 14:13:11 +0800

    deploy

--- registrar-backend/config/sanctum.php
+++ registrar-backend/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
     '%s%s',
-    'localhost,127.0.0.1:5173,localhost:5173',
+    'registrar-information-system.southeastasia.cloudapp.azure.com',
     Sanctum::currentApplicationUrlWithPort(),
 ))),
 
