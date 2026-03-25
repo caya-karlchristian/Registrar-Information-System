ca97c776d320d33aad05e3d02e6082e34205c86a
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-03-25 14:12:03 +0800

    Deploy

--- registrar-backend/config/cors.php
+++ registrar-backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'http://registrar-information-system.southeastasia.cloudapp.azure.com'],
 
     'allowed_origins_patterns' => [],
 
