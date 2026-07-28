1cdea3ec106b6540281ca7bafe3c666b4580495f
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2026-07-28 21:55:13 +0800

    feat: add tanstack/react-query

--- registrar-frontend/src/main.jsx
+++ registrar-frontend/src/main.jsx
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider';
 import { ThemeProvider } from './context/ThemeProvider';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './context/QueryClientContext';
 import App from './App';
 import './index.css';
-
+  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
